a0acd54a2442fa1650fcb90bda14a94bcfd25644
Sivaldo Vieira <vieiralmeida.com@hotmail.com>
2024-07-17 11:34:40 -0300

    Recursividade-Exemplo fatorial recursivo, realizando debug

--- AulaPratica1/Recursividade.py
+++ AulaPratica1/Recursividade.py
@@ -1,13 +1,27 @@
-#Fatorial iterativo
-def fatorial_iterativo(n):
+# #Fatorial iterativo
+# def fatorial_iterativo(n):
+#     fat = 1
+#     if n < 0:
+#         return None
+#     elif n == 0 or n == 1:
+#         return fat
+#     else:
+#         for i in range(1, n+1):
+#             fat *= i
+#         return fat
+#
+# print(fatorial_iterativo(5))
+
+
+
+#-----------------fatorial recursivo----------------
+def fatorial_recusivo(n):
     fat = 1
     if n < 0:
         return None
     elif n == 0 or n == 1:
         return fat
     else:
-        for i in range(1, n+1):
-            fat *= i
-        return fat
+        return n * fatorial_recusivo(n-1)
 
-print(fatorial_iterativo(5))
+print(fatorial_recusivo(5))
